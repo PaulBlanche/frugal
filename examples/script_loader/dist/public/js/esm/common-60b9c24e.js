a7cd22de55e2efedef7a92648e2e2d6a3b5da183
Raphael Ligier <raphael@ulule.com>
2022-01-23 11:58:08 +0100

    remove and ignore dist folders

--- examples/script_loader/dist/public/js/esm/common-60b9c24e.js
+++
@@ -1,5 +0,0 @@
-function log(message) {
-    console.log(message);
-}
-
-export { log as l };
--- examples/script_loader/dist/public/js/esm/page2-nk6r1w.js
+++
@@ -1,7 +0,0 @@
-import { l as log } from './common-60b9c24e.js';
-
-function main() {
-    log("foo");
-}
-
-main();
