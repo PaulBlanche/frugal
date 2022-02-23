a570dd4bef0754e85b0057b2238263697bf7129c
Raphael Ligier <raphael@ulule.com>
2022-02-23 15:05:48 +0100

    expose Head component

--- packages/frugal_preact/mod.client.ts
+++ packages/frugal_preact/mod.client.ts
@@ -2,4 +2,5 @@ export { useData, useUrl } from './dataContext.tsx';
 export type { AppProps } from './content.tsx';
 export { Host } from './Host.tsx';
 export { hydrate } from './hydrate.tsx';
+export { Head } from './Head.tsx'
 export type { HydrationStrategy } from './types.ts';
