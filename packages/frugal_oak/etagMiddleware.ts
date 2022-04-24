86f15fc5fbea8b0f54d6706924f7decb8bf5ebdc
Raphael Ligier <raphael@ulule.com>
2022-04-24 18:49:43 +0200

    standard log in etag middleware

--- packages/frugal_oak/etagMiddleware.ts
+++ packages/frugal_oak/etagMiddleware.ts
@@ -1,23 +1,26 @@
 import { etag, Middleware } from 'oak';
+import * as log from '../log/mod.ts';
+
+function logger() {
+    return log.getLogger(`frugal_oak:etagMiddleware`);
+}
 
 export function etagMiddleware(): Middleware {
     const baseEtagMiddleware = etag.factory();
     return async (context, next) => {
-        console.log('before etag', context.request.url.href);
         await baseEtagMiddleware(context, next);
 
-        console.log(
-            'after etag',
-            context.request.url.href,
-            context.response.headers.get('Etag'),
-        );
-
         const ifNoneMatch = context.request.headers.get('If-None-Match');
-        console.log('ifNoneMatch', context.request.url.href, ifNoneMatch);
         if (ifNoneMatch) {
             const entity = await etag.getEntity(context);
             if (entity && !etag.ifNoneMatch(ifNoneMatch, entity)) {
-                console.log('304 for', context.request.url.href);
+                logger().debug({
+                    pathname: context.request.url.pathname,
+                    msg() {
+                        return `No Etag change for ${this.pathname}, send empty 304`;
+                    },
+                });
+
                 context.response.status = 304;
                 context.response.body = '';
             }
