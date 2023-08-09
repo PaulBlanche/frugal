aab2d3a2dcf7f66cb6e6ada29479142fde87720f
Raphaël Ligier <blanchepaule@whiteshoulders.fr>
2023-08-09 14:48:50 +0200

    fix editor wiring

--- doc/src/components/code/Code.script.ts
+++ doc/src/components/code/Code.script.ts
@@ -1,28 +1,44 @@
+const REGISTERED = new WeakSet();
+
 if (import.meta.main) {
+    setup();
+
+    addEventListener("frugal:readystatechange", (event) => {
+        if (event.detail.readystate === "complete") {
+            setup();
+        }
+    });
+}
+
+function setup() {
     const editors = document.querySelectorAll<HTMLElement>("[data-code]");
 
     editors.forEach((editor) => {
-        const togglees = editor.querySelectorAll<HTMLElement>(`[data-id]`);
+        if (!REGISTERED.has(editor)) {
+            const togglees = editor.querySelectorAll<HTMLElement>(`[data-id]`);
 
-        const toggles = editor.querySelectorAll<HTMLElement>("[data-toggle-id");
+            const toggles = editor.querySelectorAll<HTMLElement>("[data-toggle-id");
 
-        toggles.forEach((toggle) => {
-            const togglee = editor.querySelector<HTMLElement>(`[data-id="${toggle.dataset.toggleId}"]`);
-            if (togglee) {
-                const toggleCode = () => {
-                    toggles.forEach((toggle) => toggle.toggleAttribute("data-active", false));
-                    togglees.forEach((togglee) => {
-                        togglee.toggleAttribute("data-active", false);
-                        togglee.setAttribute("aria-hidden", "true");
-                    });
-                    toggle.toggleAttribute("data-active", true);
-                    togglee.toggleAttribute("data-active", true);
-                    togglee.removeAttribute("aria-hidden");
-                };
+            toggles.forEach((toggle) => {
+                const togglee = editor.querySelector<HTMLElement>(`[data-id="${toggle.dataset.toggleId}"]`);
+                if (togglee) {
+                    const toggleCode = () => {
+                        toggles.forEach((toggle) => toggle.toggleAttribute("data-active", false));
+                        togglees.forEach((togglee) => {
+                            togglee.toggleAttribute("data-active", false);
+                            togglee.setAttribute("aria-hidden", "true");
+                        });
+                        toggle.toggleAttribute("data-active", true);
+                        togglee.toggleAttribute("data-active", true);
+                        togglee.removeAttribute("aria-hidden");
+                    };
 
-                toggle.addEventListener("click", toggleCode);
-                toggle.addEventListener("touch", toggleCode);
-            }
-        });
+                    toggle.addEventListener("click", toggleCode);
+                    toggle.addEventListener("touch", toggleCode);
+                }
+            });
+
+            REGISTERED.add(editor);
+        }
     });
 }
