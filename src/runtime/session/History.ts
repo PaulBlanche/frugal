cdbc8da9b41efc1e5970e3a9435969f40c7f4906
Raphaël Ligier <blanchepaule@whiteshoulders.fr>
2023-08-26 17:50:49 +0200

    fix history persistence

--- src/runtime/session/History.ts
+++ src/runtime/session/History.ts
@@ -8,19 +8,15 @@ declare global {
     }
 }
 
-const SERIALIZED_HISTORY_KEY = "frugal_browsersession_history";
-
 export class History {
     static init(config: NavigatorConfig) {
         if (HistoryInternal.instance !== undefined) {
             throw new Error("History was already initialised");
         }
 
-        const persistedHistory = sessionStorage.getItem(SERIALIZED_HISTORY_KEY);
-        if (persistedHistory) {
-            sessionStorage.removeItem(SERIALIZED_HISTORY_KEY);
-            const serializedHistory = JSON.parse(persistedHistory);
-            HistoryInternal.instance = HistoryInternal.deserialize(serializedHistory, config);
+        const restoredHistory = restoreHistory();
+        if (restoredHistory) {
+            HistoryInternal.instance = HistoryInternal.deserialize(restoredHistory, config);
         } else {
             HistoryInternal.instance = new HistoryInternal(config);
         }
@@ -54,20 +50,49 @@ export class History {
 type SerializedHistory = {
     stack: SerializedNavigator[];
     index: number;
+    current: number;
 };
 
 class HistoryInternal {
     _stack: Navigator[];
     _index: number;
+    _minIndex: number;
+    _current: number;
     _observing: boolean;
     _config: NavigatorConfig;
 
     static instance?: HistoryInternal;
 
-    static deserialize({ stack, index }: SerializedHistory, config: NavigatorConfig) {
+    static deserialize({ stack, index, current }: SerializedHistory, config: NavigatorConfig) {
         const history = new HistoryInternal(config);
         history._stack = stack.map((serialized) => Navigator.deserialize(serialized, config));
         history._index = index;
+        history._current = current;
+
+        const entries = performance.getEntriesByType("navigation");
+        switch (entries[0].type) {
+            case "navigate": {
+                if (history._index === history._current) {
+                    history._stack.push(new Navigator(new URL(location.href), config));
+                    history._current += 1;
+                } else {
+                    history._stack = history._stack.slice(0, history._current + 1);
+                    history._stack[history._current] = new Navigator(
+                        new URL(location.href),
+                        config,
+                    );
+                }
+                break;
+            }
+            case "back_forward": {
+                if (history._index === history._current) {
+                    history._current += 1;
+                }
+            }
+        }
+
+        history._minIndex = history._stack.length - 1;
+
         return history;
     }
 
@@ -75,6 +100,8 @@ class HistoryInternal {
         this._config = config;
         this._stack = [new Navigator(new URL(location.href), this._config)];
         this._index = 0;
+        this._current = 0;
+        this._minIndex = 0;
         this._observing = false;
     }
 
@@ -82,6 +109,7 @@ class HistoryInternal {
         return {
             stack: this._stack.map((navigator) => navigator.serialize()),
             index: this._index,
+            current: this._current,
         };
     }
 
@@ -92,12 +120,16 @@ class HistoryInternal {
         this._observing = true;
 
         addEventListener("beforeunload", () => {
-            sessionStorage.setItem(SERIALIZED_HISTORY_KEY, JSON.stringify(this.serialize()));
+            console.log("beforeunload");
+            persistHistory(this.serialize());
         });
 
         addEventListener("popstate", (event) => {
+            console.log("popstate", event.state);
+            this._index = this._current;
+
             const previous = this._stack[this._index];
-            const nextIndex = event.state?.index ?? 0;
+            const nextIndex = event.state?.index ?? this._minIndex;
             const current = this._stack[nextIndex];
 
             if (previous === undefined || current === undefined) {
@@ -114,6 +146,10 @@ class HistoryInternal {
             }
 
             this._index = nextIndex;
+            this._current = this._index;
+            if (this._index === this._minIndex) {
+                this._index -= 1;
+            }
 
             event.preventDefault();
 
@@ -134,10 +170,29 @@ class HistoryInternal {
     }
 
     push(navigator: Navigator) {
+        this._index = this._current;
         const stackIndex = this._index + 1;
         this._stack = this._stack.slice(0, stackIndex);
         this._stack.push(navigator);
         this._index = stackIndex;
-        history.pushState({ index: stackIndex }, "", navigator.url);
+        this._current = this._index;
+        console.log("push", { index: this._index });
+        history.pushState({ index: this._index }, "", navigator.url);
     }
 }
+
+const SERIALIZED_HISTORY_KEY = "frugal_browsersession_history";
+
+function persistHistory(serializedHistory: SerializedHistory) {
+    console.log("save", JSON.stringify(serializedHistory, null, 2));
+    sessionStorage.setItem(SERIALIZED_HISTORY_KEY, JSON.stringify(serializedHistory));
+}
+
+function restoreHistory(): SerializedHistory | undefined {
+    const persistedHistory = sessionStorage.getItem(SERIALIZED_HISTORY_KEY);
+    if (persistedHistory) {
+        return JSON.parse(persistedHistory);
+    }
+}
+
+window["TOTO"] = HistoryInternal;
